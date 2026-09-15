8e5d945ca4174721b86405127a411cdb8f59e042
inthusha20241647-commits <inthusha.20241647@iit.ac.lk>
2026-09-15 11:34:01 +0530

    updated search functionality using js

---
+++ script.js
@@ -0,0 +1,25 @@
+var input = document.getElementById("search");
+
+var card = document.querySelectorAll(".card");
+
+input.addEventListener("keyup", function (event) {
+
+    var enteredValue = event.target.value.toUpperCase();
+
+    for (count = 0; count < card.length; count++) {
+
+        var paraList = card[count].querySelector(".paraList");
+
+        if (paraList.textContent.toUpperCase().indexOf(enteredValue) < 0) {
+
+            card[count].style.display = "none";
+
+        } else {
+
+            card[count].style.display = "block";
+
+        }
+
+    }
+
+});
